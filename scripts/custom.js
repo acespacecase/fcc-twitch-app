79b613f9c434ba665054fcb18ae2ef597d46da8b
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-03-19 16:51:16 -0400

    fixed streaming links

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,20 +1,21 @@
 
-var twitchUsers = ["freecodecamp", "UnicornHole", "wolfpainter", "Monstercat", "FIRSTinspires3", "backspacenomads", "Drawwithjazza", "test_channel", "BobRoss", "Lost_In_House", "littlesiha", "Food", "ChaseSings", "CheyenneO"];
+var twitchUsers = ["freecodecamp", "comster404", "UnicornHole", "wolfpainter", "Monstercat", "FIRSTinspires3", "backspacenomads", "Drawwithjazza", "test_channel", "BobRoss", "Lost_In_House", "littlesiha", "Food", "ChaseSings", "CheyenneO"];
 
 var twitchUrl = "https://wind-bow.gomix.me/twitch-api/streams/";
 
 twitchUsers.forEach(function(user) {
   $.getJSON(twitchUrl + user + '?callback=?', function(data) {
+    console.log(data);
     if (data.stream === null) { //offline
       $('#offline').append('<div class="offline-panel col-lg-4"><div class="card"><div class="card-block"><h3>' + user
-                            + '</h3></div><div class="card-footer"><a href="' + data._links.self
+                            + '</h3></div><div class="card-footer"><a href="https://twitch.tv/' + user
                             + '" target="_blank">View Channel</a></div></div></div>');
     } else { //online
       $('#online').append('<div class="col-lg-4 online-panel"><div class="card"><img class="img-fluid card-img-top" src="'
                            + data.stream.preview.large + '"><div class="card-block"><h3>' + user
                            + '</h3><span class="status">Online Now</span><p><strong>' + data.stream.viewers
-                           + '</strong> viewers</p></div><div class="card-footer"><a href="' + data.stream._links.self
-                           + '">View Stream</a></div></div></div>');
+                           + '</strong> viewers</p></div><div class="card-footer"><a href="https://twitch.tv/' + user
+                           + '" target="_blank">View Stream</a></div></div></div>');
     };
   });
 });

e3238a2712d98c94d8a75052eab1fd146fdbab0a
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-03-19 17:30:44 -0400

    final styling

--- scripts/custom.js
+++ scripts/custom.js
@@ -9,13 +9,14 @@ twitchUsers.forEach(function(user) {
     if (data.stream === null) { //offline
       $('#offline').append('<div class="offline-panel col-lg-4"><div class="card"><div class="card-block"><h3>' + user
                             + '</h3></div><div class="card-footer"><a href="https://twitch.tv/' + user
-                            + '" target="_blank">View Channel</a></div></div></div>');
+                            + '" target="_blank">View Channel <i class="fa fa-external-link-square" aria-hidden="true"></i></a></div></div></div>');
     } else { //online
-      $('#online').append('<div class="col-lg-4 online-panel"><div class="card"><img class="img-fluid card-img-top" src="'
-                           + data.stream.preview.large + '"><div class="card-block"><h3>' + user
+      $('#online').append('<div class="col-lg-4 online-panel"><div class="card"><a href="https://twitch.tv/'
+                           + user + '" target="_blank"><img class="img-fluid card-img-top" src="'
+                           + data.stream.preview.large + '"></a><div class="card-block"><h3>' + user
                            + '</h3><span class="status">Online Now</span><p><strong>' + data.stream.viewers
                            + '</strong> viewers</p></div><div class="card-footer"><a href="https://twitch.tv/' + user
-                           + '" target="_blank">View Stream</a></div></div></div>');
+                           + '" target="_blank">View Stream <i class="fa fa-external-link-square" aria-hidden="true"></i></a></div></div></div>');
     };
   });
 });
